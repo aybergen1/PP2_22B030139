98fb6445ffcd349438dfc4ddf994172a585f147e
aybergen1 <123065857+aybergen1@users.noreply.github.com>
2023-02-08 23:22:17 +0600

    Delete lab_03/function directory

--- lab_03/function/Fahrenheit.py
+++
@@ -1,7 +0,0 @@
-def qaws(d):
-    C = (5/9)*(d-32)
-    return C
-
-
-F = int(input())
-print(qaws(F))
--- lab_03/function/puzzle.py
+++
@@ -1,8 +0,0 @@
-def solve(numheads, numlegs):
-    x = int((numlegs - 2 * numheads) / 2)
-    y = int(numheads - x)
-    ans = f"{x} rabbits,{y} chickens"
-    return ans
-numheads = int(input("number heads:"))
-numlegs = int(input("number legs:"))
-print(solve(numheads,numlegs))
