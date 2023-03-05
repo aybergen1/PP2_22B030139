718fb26da315fdfc2a7adb0ed674a967a2f8fbe0
Aybergen <a_abzhal@kbtu.kz>
2023-03-06 00:45:30 +0600

    TSIS 6

--- lab_06/dir/dir5.py
+++ lab_06/dir/dir5.py
@@ -1,9 +1,9 @@
 import os
 wer = ["apple", "banana", "cherry", "durian"]
 
-with open("qqqqq1.txt", "w") as fi:
+with open("qwe.txt", "w") as fi:
     for i in wer:
         fi.write(i + "\n")
 
-rew= open("qqqqq1.txt", "r")
+rew= open("qwe.txt", "r")
 rew.read()
--- lab_06/dir/dir7.py
+++ lab_06/dir/dir7.py
@@ -1,6 +1,6 @@
 import os
 
-qw =open("qqqqq1.txt", "r")
+qw =open("qwe.txt", "r")
 we =open("qqqqq.txt", "w")
 for i in qw:
     we.write(str(i))
